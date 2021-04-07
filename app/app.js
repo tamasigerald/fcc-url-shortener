16bb5118e6656460310201310d09214e1ff1d559
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-04-07 16:13:35 +0200

    init

--- app/app.js
+++ app/app.js
@@ -28,7 +28,13 @@ async function newUrl(req, res) {
     const {body} = req;
     try {
         let url = body.url;
-        const checkUrl = await urlExists(url);
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        const checkUrl = pattern.test(str);
         if (checkUrl === false) {
             throw error
         }
